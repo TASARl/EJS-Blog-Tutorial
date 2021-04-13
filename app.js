b72cb2cb299cbf40b6ad0102d1c0d254b98f1d88
HamdiYILMAZ <49573532+hamdi-yilmaz@users.noreply.github.com>
2021-04-14 02:38:14 +0300

    adres satırından değer alma

--- app.js
+++ app.js
@@ -45,6 +45,10 @@ app.post("/compose", (req, res) => {
     res.redirect("/");
 });
 
+app.get("/posts/:postName", (req, res) => {
+    console.log(req.params.postName); // adres satırından değer alma (get e dikkat!)
+});
+
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
